c96d6628bb83d6a7c3df99806d0242e1fdfbc525
yeoleummoon <yoruminn@naver.com>
2022-08-26 23:12:01 +0900

    mailbutton

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -13,7 +13,7 @@ const Home = (props) => {
           <p className={styles.developer}>Frontend Developer</p>
       </div>
       <section className={styles.info}>
-          <p className={styles.iam}>끊임없이 고민하고 성장하고 싶습니다</p>
+          <p className={styles.iam}>열매를 열리게 하는 개발자로</p>
             <div className={styles.button}>
               <button className={styles.resume}>RESUME</button>
               <button className={styles.project} onClick={() => {
@@ -21,9 +21,9 @@ const Home = (props) => {
               }}>PROJECT</button>
             </div>
             <div className={styles.story}>
-            <p>Frontend를 깊이 있게 다져나가고자 하는 주니어 개발자입니다.</p>
-            <p>그렇게 다양한 경험을 쌓았으며, 매번 새로운 계획과 도전은 큰 자극제가 되어 주었습니다.</p>
-            <p>스스로에게 새로운 과제를 내어주며 계속해서 발전해 나갈 기회를 만들어주고자 합니다.</p>
+            <p>Frontend를 깊이 있게 다져나가고자 주니어 개발자에 지원합니다.</p>
+            <p>다양한 경험을 쌓았으며, 매번 새로운 계획과 도전은 큰 자극제가 되어 주었습니다.</p>
+            <p>앞으로도 스스로에게 새로운 과제를 내어주며 계속해서 발전해 나갈 기회를 만들어주고자 합니다.</p>
             <div className={styles.contact}>
               <button><a href="mailto:yoruminn@naver.com"><img src="../../images/icon_mail.png" alt="mail" /></a></button>
             </div>
